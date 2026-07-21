3cdeda36db1f142b565dbdeec1291eae90d5a15c
Matthew Pham <matthewpham03@gmail.com>
2026-07-21 13:20:47 -0700

    Implement expected goals

--- simulate.py
+++ simulate.py
@@ -8,4 +8,13 @@ N_SIMULATIONS = 10_000
 def expected_goals(rating_home: float, rating_away: float, is_home: int,
                    home_model: PoissonRegressor, away_model: PoissonRegressor,
                    scaler: StandardScaler) -> tuple[float, float]:
-    pass
+    rating_diff = rating_home - rating_away
+
+    X_df = pd.DataFrame([{"rating_diff": rating_diff, "is_home": is_home}])
+
+    X_scaled_df = scaler.transform(X_df)
+
+    lambda_home = home_model.predict(X_scaled_df)[0]
+    lambda_away = away_model.predict(X_scaled_df)[0]
+
+    return lambda_home, lambda_away

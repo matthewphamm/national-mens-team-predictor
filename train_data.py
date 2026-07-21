6ff2981ade7ac7f40bdad55274c51a277df47c91
Matthew Pham <matthewpham03@gmail.com>
2026-07-21 11:55:13 -0700

    Create function

--- train_data.py
+++ train_data.py
@@ -1,3 +1,7 @@
 import pandas as pd
 from data_prep import load_data, DATA_PATH
 from elo import build_ratings, STARTING_RATING
+
+def build_training_df(df: pd.DataFrame) -> pd.DataFrame:
+    pass
+
